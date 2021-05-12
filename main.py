ca8ed5773e02314fcf78314a773c27fca0c8c2aa
Alexsander Uglin <uglin76@mail.ru>
2021-05-12 07:47:29 +0300

    solution

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from application.cls_phonebook import PhoneBook
 
 def main():
     raw_data_path = 'application/data/phonebook_raw.csv'
-    phone_book = PhoneBook(raw_data_path)
+    phonebook = PhoneBook(raw_data_path)
 
     pure_data_path = 'application/data/phonebook_pure.csv'
-    phone_book.fix(pure_data_path)
+    phonebook.fix(pure_data_path)
 
 
 if __name__ == '__main__':
