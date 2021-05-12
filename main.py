5e33e3835d1183d0dba129f4ee43c3c05ebb83a6
Alexsander Uglin <uglin76@mail.ru>
2021-05-12 07:49:40 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from application.cls_phonebook import PhoneBook
 
 def main():
-    raw_data_path = 'application/data/phonebook_raw.csv'
-    phonebook = PhoneBook(raw_data_path)
-
-    pure_data_path = 'application/data/phonebook_pure.csv'
-    phonebook.fix(pure_data_path)
+    phonebook = PhoneBook('application/data/phonebook_raw.csv')
+    phonebook.fix('application/data/phonebook_pure.csv')
 
 
 if __name__ == '__main__':
